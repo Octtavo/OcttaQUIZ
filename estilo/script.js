c2be5ea1ce993b0ef3ffae2a0494beb9b332022e
Octtavo <aotavio061@gmail.com>
2022-02-10 18:12:10 -0300

    Testando meu JS

--- estilo/script.js
+++ estilo/script.js
@@ -1,3 +1,9 @@
 function comecar() {
-    
+    window.location.href = '../fase1/primeirafase.html'
+}
+function fases() {
+    window.location.href = '../paineldefases/fases.html'
+}
+function info() {
+    window.location.href = '../creditos/creditos.html'
 }
