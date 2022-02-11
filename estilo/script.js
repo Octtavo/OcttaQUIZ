34babc6ee8f2a2bfaabe470d74be4957a83b9000
Octtavo <aotavio061@gmail.com>
2022-02-11 20:57:50 -0300

    Organizando os meus documentos para receberem o javascript

--- estilo/script.js
+++ estilo/script.js
@@ -1,5 +1,7 @@
+      //Configuração da página inicial
 
-
+alert("Olá, aqui vai um pequeno aviso aos novos jogadores, antes de tudo a companhia OcttaQUIZ pede que leiam as descrições, basta clicar no i (de informação) que está logo abaixo")
+            
 function comecar() {
     window.location.href = './fase1/primeirafase.html'
 }
