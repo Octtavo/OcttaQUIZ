5e7ef0b41ff05e144a8000665518eebf61eee685
Octtavo <aotavio061@gmail.com>
2022-02-11 23:53:38 -0300

    Dando continuidade a inserção de JS ao material

--- estilo/script.js
+++ estilo/script.js
@@ -1,4 +1,4 @@
-      //Configuração da página inicial
+                                                   //Configuração da página inicial
 
 alert("Olá, aqui vai um pequeno aviso aos novos jogadores, antes de tudo a companhia OcttaQUIZ pede que leiam as descrições, basta clicar no i (de informação) que está logo abaixo")
             
@@ -11,3 +11,58 @@ function fases() {
 function info() {
     window.location.href = './creditos/creditos.html'
 }
+                                                   //Configuração da página inicial
+
+
+
+                                                    //CONFIGURAÇÃO DO PAINEL DE FASES
+                                            
+function botao1() {
+    window.location.href = '../fase1/primeirafase.html'
+}
+function botao2() {
+    window.location.href = '../fase2/segundafase.html'
+}
+function botao3() {
+    window.location.href = '../fase3/terceirafase.html'
+}
+function botao4() {
+    window.location.href = '../fase4/quartafase.html'
+}
+function botao5() {
+    window.location.href = '../fase5/quintafase.html'
+}
+function botao6() {
+    window.location.href = '../fase6/sextafase.html'
+}
+function botao7() {
+    window.location.href = '../fase7/setimafase.html'
+}
+function botao8() {
+    window.location.href = '../fase8/oitavafase.html'
+}
+function botao9() {
+    window.location.href = '../fase9/nonafase.html'
+}
+function botao10() {
+    window.location.href = '../fase10/decimafase.html'
+}
+
+function retornarpainel() {
+    window.location.href = '../index.html'
+}
+                                                      //CONFIGURAÇÃO DO PAINEL DE FASES
+
+
+
+
+                                                      //CONFIGURAÇÃO DA AREA DE CREDITOS
+function retornarcreditos() {
+    window.location.href = '../index.html'
+}
+
+                                                       //CONFIGURAÇÃO DA AREA DE CREDITOS
+
+
+
+                                    
